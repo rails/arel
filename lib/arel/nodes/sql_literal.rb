7e753a25fb6962adf9be287925b8940f0867bd95
Maurizio De Santis <desantis.maurizio@gmail.com>
2013-03-04 15:49:59 +0100

    Custom encode_with and init_with (fixes #149)
    It declares encode_with and init_with for Arel::Nodes::SqlLiteral, which restores the dump / load with Psych.

--- lib/arel/nodes/sql_literal.rb
+++ lib/arel/nodes/sql_literal.rb
@@ -5,6 +5,14 @@ module Arel
       include Arel::Predications
       include Arel::AliasPredication
       include Arel::OrderPredications
+       
+      def encode_with(coder)
+        coder['string'] = self.to_s
+      end
+      
+      def init_with(coder)
+        self << coder['string']
+      end
     end
 
     class BindParam < SqlLiteral
