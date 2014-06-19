02d4d6c8bd7c5138acc89b0c4748e4303a639df2
JoseLuis Torres <jl@joseluistorres.me>
2014-06-18 22:48:59 -0500

    adding the FROM to postgreSQL UPDATE with inner joins related to #111

--- lib/arel/visitors/postgresql.rb
+++ lib/arel/visitors/postgresql.rb
@@ -23,6 +23,40 @@ module Arel
         collector << "DISTINCT ON ( "
         visit(o.expr, collector) << " )"
       end
+
+      def visit_Arel_Nodes_UpdateStatement o, collector
+        if o.orders.empty? && o.limit.nil?
+          wheres = o.wheres
+        else
+          wheres = [Nodes::In.new(o.key, [build_subselect(o.key, o)])]
+        end
+
+        collector << "UPDATE "
+
+        if o.relation.right && o.relation.right.first.instance_of?(Arel::Nodes::InnerJoin) && o.cores.first.source.empty?
+          o.cores = o.relation
+          collector = visit o.relation.left, collector
+        else
+          collector = visit o.relation, collector
+        end
+
+        unless o.values.empty?
+          collector << " SET "
+          collector = inject_join o.values, collector, ", "
+        end
+
+        unless o.cores.empty? && o.cores.right.empty?
+          collector << " FROM "
+          collector = visit o.cores.right.first.left, collector
+        end
+
+        unless wheres.empty?
+          collector << " WHERE "
+          collector = inject_join wheres, collector, " AND "
+        end
+
+        collector
+      end
     end
   end
 end
