7b823819f7b1d71a23f884184df7c83a0d40de1b
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-02-06 00:08:24 +0900

    Fix column_cache

--- lib/arel/visitors/to_sql.rb
+++ lib/arel/visitors/to_sql.rb
@@ -130,12 +130,20 @@ module Arel
 
       def column_for attr
         return unless attr
-        name    = attr.name.to_s
-        table   = attr.relation.table_name
+        name     = attr.name.to_s
+        relation = attr.relation
 
-        return nil unless table_exists? table
+        if has_columns_hash? relation
+          relation.engine.columns_hash[name]
+        elsif table_exists? relation.table_name
+          column_cache(relation.table_name)[name]
+        else
+          nil
+        end
+      end
 
-        column_cache(table)[name]
+      def has_columns_hash?(relation)
+        relation.respond_to?(:engine) && relation.engine.respond_to?(:columns_hash)
       end
 
       def column_cache(table)
