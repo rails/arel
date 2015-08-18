750108cc1150152cc75da64a736e536592e78aa0
Ben A. Morgan <ben@benmorgan.io>
2015-08-18 00:41:23 -0700

    use WHERE

--- lib/arel/visitors/to_sql.rb
+++ lib/arel/visitors/to_sql.rb
@@ -77,7 +77,7 @@ module Arel
         collector << 'DELETE FROM '
         collector = visit o.relation, collector
         if o.wheres.any?
-          collector << ' WHERE '
+          collector << WHERE
           collector = inject_join o.wheres, collector, AND
         end
 
@@ -111,7 +111,7 @@ module Arel
         end
 
         unless wheres.empty?
-          collector << " WHERE "
+          collector << WHERE
           collector = inject_join wheres, collector, AND
         end
 
