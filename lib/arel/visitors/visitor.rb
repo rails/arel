2c5b0efa2e5e79dc79f72b307b97010b14f4f09e
Brandon Dewitt <brandonsdewitt@gmail.com>
2017-01-13 11:09:11 -0700

    attempted test for legitimate NoMethodError in visitor#visit

--- lib/arel/visitors/visitor.rb
+++ lib/arel/visitors/visitor.rb
@@ -27,28 +27,23 @@ module Arel
         @dispatch
       end
 
-      def visit object
+      def dispatch_method(object)
         send dispatch[object.class], object
+      end
+
+      def visit object
+        dispatch_method(object)
       rescue NoMethodError => e
         superklass = object.class.ancestors.find { |klass|
           respond_to?(dispatch[klass], true)
         }
-
         raise(TypeError, "Cannot visit #{object.class}") unless superklass
 
         NO_METHOD_MUTEX.synchronize do
           dispatch[object.class] = dispatch[superklass] unless respond_to?(dispatch[object.class], true)
         end
 
-        if respond_to?(dispatch[object.class], true)
-          begin
-            send dispatch[object.class], object
-          rescue NoMethodError => e2
-            raise e2
-          end
-        else
-          raise e
-        end
+        dispatch_method(object)
       end
     end
   end
