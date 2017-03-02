b6ad369df77cbc956a2844c0738a0446f3f4c70b
Alex Moreno <alexmobo@gmail.com>
2017-03-02 23:36:20 +0100

    Added default random name to temporary table

--- lib/arel/temporary_table.rb
+++ lib/arel/temporary_table.rb
@@ -3,9 +3,15 @@ module Arel
   class TemporaryTable < Table
     attr_accessor :data_source
 
-    def initialize(data_source, as:, type_caster: nil)
+    def initialize(data_source, as: default_name, type_caster: nil)
       super(as, type_caster: type_caster)
       @data_source = data_source
+    end
+
+    private
+
+    def default_name
+      Array.new(10) { Random.rand(97..122) }.map(&:chr).join
     end
   end
 end
--- test/test_temporary_table.rb
+++ test/test_temporary_table.rb
@@ -14,6 +14,11 @@ module Arel
         rel.name.must_equal 'users'
       end
 
+      it 'sets the name with a 10-length random name' do
+        rel = TemporaryTable.new 'SELECT * FROM clients'
+        rel.name.must_match(/[a-z]{10}/)
+      end
+
       it 'does not set any alias' do
         rel = TemporaryTable.new 'SELECT * FROM clients', as: 'users'
         rel.table_alias.must_be_nil
