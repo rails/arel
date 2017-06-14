4b70440286e76c068fb4f9f0b50f86c63ee15b53
Alex Moreno <alexmobo@gmail.com>
2017-06-14 08:42:58 +0200

    tests changed

--- test/test_subquery.rb
+++ test/test_subquery.rb
@@ -2,25 +2,25 @@
 require 'helper'
 
 module Arel
-  describe TemporaryTable do
+  describe Subquery do
     describe 'new' do
       it 'sets the data_source' do
-        rel = TemporaryTable.new 'SELECT * FROM clients', as: 'users'
+        rel = Subquery.new 'SELECT * FROM clients', as: 'users'
         rel.data_source.must_equal 'SELECT * FROM clients'
       end
 
       it 'sets the name with the as attribute' do
-        rel = TemporaryTable.new 'SELECT * FROM clients', as: 'users'
+        rel = Subquery.new 'SELECT * FROM clients', as: 'users'
         rel.name.must_equal 'users'
       end
 
       it 'sets the name with a 10-length random name' do
-        rel = TemporaryTable.new 'SELECT * FROM clients'
+        rel = Subquery.new 'SELECT * FROM clients'
         rel.name.must_match(/[a-z]{10}/)
       end
 
       it 'does not set any alias' do
-        rel = TemporaryTable.new 'SELECT * FROM clients', as: 'users'
+        rel = Subquery.new 'SELECT * FROM clients', as: 'users'
         rel.table_alias.must_be_nil
       end
     end
