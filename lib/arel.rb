6a3e12f85cdca253fea87b7133c86679a24e4d88
Alex Moreno <alexmobo@gmail.com>
2017-06-14 08:40:02 +0200

    require changed

--- lib/arel.rb
+++ lib/arel.rb
@@ -11,7 +11,7 @@ require 'arel/math'
 require 'arel/alias_predication'
 require 'arel/order_predications'
 require 'arel/table'
-require 'arel/temporary_table'
+require 'arel/subquery'
 require 'arel/attributes'
 require 'arel/compatibility/wheres'
 
